c107c6e3c6a16d0ed0fbbabc624a5c75cf743e64
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-23 05:39:08 +0800

    Update alone.sh

--- alone.sh
+++ alone.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-sudo -i
-
+# 下载并解压 Titan 文件
 wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.14/titan_v0.1.14_linux_amd64.tar.gz
 tar -zxvf titan_v0.1.14_linux_amd64.tar.gz
 
-sleep 20
+# 等待10秒
+sleep 10
 
+# 赋予执行权限
 chmod u+x titan_v0.1.14_linux_amd64
-cd titan_v0.1.14_linux_amd64
 
-tmux new -s titan
+# 创建tmux会话并启动 Titan daemon
+tmux new -s titan -d "cd titan_v0.1.14_linux_amd64 && ./titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0"
 
-./titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0
-./titan-edge bind --hash=369BF4FD-0A3F-4EEF-B280-CE7A9C527907 https://api-test1.container1.titannet.io/api/v2/device/binding
+# 等待5秒
+sleep 5
+
+# 在后台创建新的tmux会话并执行 bind 操作
+tmux new -s titan_bind -d "cd titan_v0.1.14_linux_amd64 && ./titan-edge bind --hash=369BF4FD-0A3F-4EEF-B280-CE7A9C527907 https://api-test1.container1.titannet.io/api/v2/device/binding"
